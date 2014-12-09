28e317adcca6f7dcda7b63fbb87b76b382e86ce0
Zoltán Sasvári <z.sasvari@student.tugraz.at>
2014-12-09 16:31:52 +0100

    bugs fixed

--- A8/speed.c
+++ A8/speed.c
@@ -1,31 +1,28 @@
 #include <stdio.h>
 #include <time.h>
+#include <unistd.h>
 
 int get_cpu_speed() {
   // TODO: this function should return the clock speed of the
   // CPU in MHz
-  uint32_t low, high;
+  unsigned int low, high;
   asm (
        "cpuid;" //serializing instruction: force CPU to in-order execution
        "rdtsc;"
        : "=a"(low), "=d"(high)
        :
        );
-  uint64_t ts_counter1 = ((uint64_t)high << 32) | low;
+  unsigned long long ts_counter1 = ((unsigned long long)high << 32) | low;
   //wait
-  time_t time_a = time(NULL);
-  time_t time_b = time(NULL);
-  while (difftime(time_b, time_a) < 1) {
-    time_b = time(NULL);
-  }
+  sleep(1);
   asm (
        "cpuid;"
        "rdtsc;"
        : "=a"(low), "=d"(high)
        :
        );
-  uint64_t ts_counter2 = ((uint64_t)high << 32) | low;
-  uint64_t cycles = ts_counter2 - ts_counter1;
+  unsigned long long ts_counter2 = ((unsigned long long)high << 32) | low;
+  unsigned long long cycles = ts_counter2 - ts_counter1;
   return (int)(cycles / 1000000);
 }
 
